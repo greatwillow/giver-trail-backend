8a179b9fb329777fa5e8e72df23c7f8ca92f9f5a
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-11-13 20:51:41 -0500

    API call to fetch some geoData , and store it to the database and a route to call them

---
+++ server/models/trails.1.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const _ = require('lodash');
+var { Mongoose } = require('../db/mongoose.js');
+const express = require('express');
+const { User } = require('../models/users');
+var bodyParser = require('body-parser');
+var app = express();
+
+const { ObjectID } = require('mongodb');
+const { authenticate } = require('../middleware/authenticate');
+
+const axios = require('axios');
+const port = process.env.PORT || 3000;
+
+
+var trailsSchema = new mongoose.Schema({
+
+    type: {
+        type: String
+    },
+    id: {
+        type: String
+    },
+    tags: {
+
+    },
+    bounds: {
+        minlon: {
+            type: String
+        },
+        minlat: {
+            type: String
+        },
+        maxlon: {
+            type: String
+        },
+        maxlat: {
+            type: String
+        },
+
+    },
+    geometry: {
+        lat: [{
+            type: String
+        }],
+        lon: [{
+            type: String
+        }]
+    },
+    nodes: [{
+        type: String
+    }]
+
+});
+
+
+trailsSchema.methods.toJSON = function() {
+    var trail1 = this;
+    var trailObject = trail1.toObject();
+    return _.pick(trailObject, ['type', 'id', 'tags', 'geometry', 'minlat', 'maxlat', 'minlon', 'maxlon', 'lat', 'lon', 'nodes', 'bounds']);
+};
+
+trailsSchema.statics.findByName = function(name) {
+
+    trail1 = this;
+    return trail1.findOne({
+        'name': name
+    })
+}
+var Trails1 = mongoose.model('trails1', trailsSchema);
+module.exports = {
+    Trails1
+}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { User } = require('./models/users');
 const { Points } = require('./models/points');
 const { Trails } = require('./models/trails');
+const { Trails1 } = require('./models/trails.1');
 const { Trips } = require('./models/trip');
 var bodyParser = require('body-parser');
 var app = express();
@@ -100,43 +101,128 @@ app.post('/users-points-trail-trip', (req, res) => {
 
 });
 
-// app.get('/getTrails', (req, res) => {
-//     const url = 'http://overpass-api.de/api/interpreter?data=[out:json];way["highway"="footway"](50.745,7.17,50.75,7.18);out geom;';
-//     console.log('inside get trails')
-
-//     axios.get(url).then((res) => {
-//         var cb = (err, result) => {
-//             if (err) {
-//                 console.log(err);
-//             }
-//             console.log(result);
-
-//         };
+app.get('/getTrails', (req, res) => {
+    const url = 'http://overpass-api.de/api/interpreter?data=[out:json];way["highway"="footway"](50.745,7.17,50.75,7.18);out geom;';
+    console.log('inside get trails')
 
 
-//         res.data.elements.map((element) => {
-//             //  console.log(`${JSON.stringify(element.tags.highway)} ${JSON.stringify(element.geometry)}`);
-//             // let locations = [{ lat: 50.7482984, lng: 7.1724735 }, { lat: 50.748259, lng: 7.172773 }];
-
-//             let locations = element.geometry;
-
-//             console.log('before#########', locations);
-
-//             let modifiedResults = locations.map((e) => {
-
-//                 return { lat: e.lat, lng: e.lon }
-//             })
-
-//             console.log('after#########', modifiedResults);
+    axios.get(url).then((data) => {
 
 
+        // let finalData = data.data.elements.map((element) => {
 
-//             elevation.locations(modifiedResults, null, cb);
+        // let location = [element];
 
-//         });
-//     });
+        // return modifiedResults = location.filter((e) => {
+        //     return { type: e.type, id: e.id, tags: e.tags, geometry: e.geometry, bounds: e.bounds }
 
-// })
+        // })
+
+        //console.log(modifiedResults);
+
+        // let modifiedResults1 = modifiedResults.geometry.map((e) => {
+        //     return { lat: e.lat, lng: e.lon }
+        // })
+
+        //console.log(modifiedResults);
+        // elevation.locations(modifiedResults, null, (err, result) => {
+        //     if (err) {
+        //         console.log(err);
+        //     }
+        //     //  console.log(result);
+        //     // query data (data.lat = result.lt)
+        //     // push data.push(result)
+
+        // });
+
+
+        // });
+        let count = 0;
+        let r = {
+
+                element: data.data.elements.map((e) => {
+
+
+                    return { thename: e }
+                })
+
+            }
+            // console.log(r.element[3].thename.type);
+
+
+        r.element.filter((e) => {
+            let bodyTrail = _.pick(e.thename, ['type', 'id', 'bounds', 'tags']);
+            let geometry = _.pick(e.thename, ['geometry']);
+            let nodes = _.pick(e.thename, ['nodes']);
+
+            let newData = Object.assign(bodyTrail, geometry, nodes);
+
+            let trail1 = new Trails1(newData);
+            trail1.id = newData.id;
+            // console.log(trail1.id); // id 
+            // console.log(newData.id); //
+            trail1.type = newData.type;
+            // console.log(trail1.type); // type 
+            // console.log(newData.type); //
+
+            // console.log('bounds data ', newData.bounds);
+
+            trail1.bounds.minlat = newData.bounds.minlat;
+            // console.log('minlat trail ', trail1.bounds.minlat); // minlat
+
+            trail1.bounds.minlon = newData.bounds.minlon;
+            // console.log('minlon trail ', trail1.bounds.minlon); //minlon
+
+            trail1.bounds.maxlat = newData.bounds.maxlat;
+            // console.log('maxlat trail ', trail1.bounds.maxlat); //maxlat
+
+            trail1.bounds.maxlon = newData.bounds.maxlon;
+            // console.log('maxlon trail ', trail1.bounds.maxlon); //maxlon
+
+
+            trail1.tags = newData.tags;
+            // console.log(trail1.tags); //
+            // console.log(newData.tags); //
+            let lat = newData.geometry.map((geo) => {
+                // console.log('lat data : ', geo.lat)
+                return geo.lat;
+            });
+            trail1.geometry.lat = lat;
+            // console.log('lat trail : ', trail1.geometry.lat)
+            let lon = newData.geometry.map((geo) => {
+                // console.log('lon data : ', geo.lon)
+                return geo.lon;
+            });
+            trail1.geometry.lon = lon;
+            // console.log('lon trail : ', trail1.geometry.lon)
+            trail1.nodes = newData.nodes;
+            // console.log('nodes trail : ', trail1.nodes); //
+            // console.log('nodes data : ', newData.nodes); //
+            console.log('before save ')
+
+            trail1.save().then((dataSaved) => {
+
+            }).catch((err) => { res.status(401).send(err) });
+
+
+        });
+
+        res.status(200).send();
+
+    });
+
+
+})
+
+app.get('/showtrails', (req, res) => {
+    let allTrails = [];
+    let count = 0;
+    Trails1.find().then((data) => {
+
+        res.send(data);
+    });
+
+})
 
 app.get('/getPoint', (req, res) => {
     var obj = {
